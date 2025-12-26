45f174d08477de82f76ca2f83706f6c2a9a8a565
OM <omjaiswal2552@gmail.com>
2025-12-26 23:49:10 +0530

    modified index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import chatRoutes from './routes/chatRoutes'
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 
 app.use('/chat', chatRoutes);
 
