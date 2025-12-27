5042ffb2cfbfbba818df60f7ec3a65f94e156fc9
OM <omjaiswal2552@gmail.com>
2025-12-27 14:21:12 +0530

    modified app.tsx ..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -53,10 +53,12 @@ function App() {
       createdAt: new Date().toISOString(),
       status: 'sending'
     }
-    setMessages(prev => [...prev, tmpUsrMsg])
+    setMessages(prev => [...prev, tmpUsrMsg]);
+    const msgToSend = inputText;
+    setInputText('');
     try {
       const res = await axios.post(`${apiUrl}/chat/message`, {
-        message: inputText,
+        message: msgToSend,
         sessionId: sessionId || undefined
       })
       if (res.data.reply) {
@@ -83,7 +85,7 @@ function App() {
         nextCursor
       })
       if (res.data.success) {
-        setMessages(res.data.data);
+        setMessages(prev => [res.data.data, ...prev]);
         setNextCursor(res.data.nextCursor);
       }
     } catch (err) {
@@ -97,8 +99,19 @@ function App() {
       <h1 className='text-2xl font-semibold'>Customer Support Chat</h1>
       <div className='bg-gray-300 h-full w-full max-w-4xl rounded-2xl shadow-lg relative'>
 
-        <div className=''>
-          hiiiiiiiiiiiiii
+        <div className='flex flex-col flex-1 overflow-y-auto gap-2 px-2 py-4'>
+          {messages.map((msg) => (
+            <div 
+              key={msg.id}
+              className={`flex ${msg.sender === 'User' ? 'justify-end' : 'justify-start'}`}
+            >
+              <p
+              className={`px-4 py-2 text-sm lg:text-lg rounded-2xl shadow max-w-[70%] text-start ${msg.sender === 'User' ? 'bg-blue-200' :  'bg-blue-100'}`}
+              >
+                {msg.text}
+              </p>
+            </div>
+          ))}
         </div>
 
         <div className='absolute inset-x-0 bottom-0 space-x-2 pb-8'>
@@ -109,6 +122,7 @@ function App() {
               placeholder='Type your message...'
               className='px-3 py-2 w-2xl bg-white rounded-2xl' 
               onChange={(e) => setInputText(e.target.value)}
+              onKeyDown={(e) => e.key === 'Enter' && !e.shiftKey && (e.preventDefault(), handleSend())}
             />
             <button 
               className='px-6 py-2 bg-gray-800 text-white rounded-2xl hover:scale-105'
